e78813dd3ca49c5d951bf30d8b9d3b6e375a1852
140ch204 <140ch204_github@gadz.org>
2019-10-30 20:16:22 +0100

    V2

---
+++ app/controllers/city_controller.rb
@@ -0,0 +1,42 @@
+class CityController < ApplicationController
+
+  def new
+    # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
+    # Gossip.new
+  end
+
+  def create
+    # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
+    # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
+    # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
+
+  end
+
+  def show
+    # Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
+    @city=City.find(params[:id])
+  	@gossip_city=Gossip.where(user_id:@city.users.ids)
+  end
+
+  def index
+    # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
+
+  end
+
+  def edit
+    # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
+  end
+
+  def update
+    # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
+    # pour info, le contenu de ce formulaire sera accessible dans le hash params
+    # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
+  end
+
+  def destroy
+    # Méthode qui récupère le potin concerné et le détruit en base
+    # Une fois la suppression faite, on redirige généralement vers la méthode index (pour afficher la liste à jour)
+  end
+
+
+end
