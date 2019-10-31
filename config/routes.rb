5cf19c87515b3def771f3561352da836d9c9ecd1
140ch204 <140ch204_github@gadz.org>
2019-10-31 10:17:27 +0100

    V2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-  
   
   root 'gossip#index'
   resources :gossip do
@@ -12,17 +10,4 @@ Rails.application.routes.draw do
   resources :welcome, only: [:index, :show]
   resources :city, only: [:show]
 
-  
-
-  #______OLD1___________#
-
-  
-  #get '/author/:id', to: 'author#show'
-  #get '/gossip/:id', to: 'gossip#show'
-  #get '/gossip', to: 'landing#page'
-  #get '/', to: 'landing#page'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get '/welcome/:first_name', to: 'welcome#user'
-  #get '/contact', to: 'static_pages#contact'
-  #get '/team', to: 'static_pages#team'
 end
